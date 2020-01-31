1c08ade709e424e943a8da7c814622d23ea14983
Mikira Sora <mikirasora0409@126.com>
2020-02-01 00:25:30 +0800

    fix tag type

--- GeneralizableMoebooruAPI/Bases/TagType.cs
+++ GeneralizableMoebooruAPI/Bases/TagType.cs
@@ -6,6 +6,6 @@ namespace GeneralizableMoebooruAPI.Bases
 {
     public enum TagType
     {
-        General, Artist, Character, Copyright,/* Ambiguous,*/Unknown, Circle, Faults
+        General = 0, Artist = 1, Character = 4, Copyright = 3,/* Ambiguous,*/Unknown = -2857, Circle = 5, Faults = 6
     }
 }
